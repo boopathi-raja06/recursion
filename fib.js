dc9c535598253a64e0f2c88dd777d25a0ded3a65
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-04-24 21:24:26 +0530

    finish recursion

--- fib.js
+++ fib.js
@@ -1,3 +1,23 @@
-function fibs(n){
-    
+function fibs(n) {
+  const res = [0, 1];
+  for (i = 2; i < n; i++) {
+    let temp = parseInt(res[i - 2]) + parseInt(res[i - 1]);
+    res.push(temp);
+  }
+  return res;
 }
+console.log(fibs(8));
+function fib_rec(n) {
+  if (n === 1) {
+    return [0];
+  } else if (n === 2) {
+    return [0,1];
+  } else {
+    
+    res=fib_rec(n-1);
+    res.push(res[res.length-1]+res[res.length-2])
+    return res ;
+  }
+}
+console.log("***************************");
+console.log(fib_rec(8));
