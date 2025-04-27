eebb110dbf3bd05d8d6e8b6adc6eb5d163d9e78d
boopathiraja-06 <boopathiraja060700@gmail.com>
2025-04-27 20:59:42 +0530

    finish sorting

--- mergesort.js
+++ mergesort.js
@@ -1,44 +1,44 @@
-function mergesort(array,s,e){
-    let mid=Math.floor((s+e)/2);
-    if(s<e){
-        mergesort(array,s,mid)
-        mergesort(array,mid+1,e)
-        merge(array,s,mid,e)
+function mergesort(array, s, e) {
+    if (s < e) {
+        let mid = Math.floor((s + e) / 2);
+        mergesort(array, s, mid);
+        mergesort(array, mid + 1, e);
+        merge(array, s, mid, e);
+    }
 }
-};
-function merge(array,s,mid,e)
-{
-    let firsthalf=[]
-    let secondhalf=[]
-    let i=0
-    let j=0
-    let k=s
-    for(i=0;i<=mid;i++,k++){
-        firsthalf[i]=array[k]
-    }
-    for(j=0;j<=e;j++,k++){
-        secondhalf[j]=array[k]
-    }
-    k=s;
-    i=0;
-    j=0;
 
-    while(i<=firsthalf.length && j<=secondhalf.length)
-    {
-        if(firsthalf[i]<secondhalf[j]){
-            array[k++]=firsthalf[i++]
+function merge(array, s, mid, e) {
+    let firsthalf = [];
+    let secondhalf = [];
+
+    // Correct way to copy elements
+    for (let i = s; i <= mid; i++) {
+        firsthalf.push(array[i]);
+    }
+    for (let i = mid + 1; i <= e; i++) {
+        secondhalf.push(array[i]);
+    }
+
+    let i = 0, j = 0, k = s;
+
+    // Correct while condition
+    while (i < firsthalf.length && j < secondhalf.length) {
+        if (firsthalf[i] < secondhalf[j]) {
+            array[k++] = firsthalf[i++];
+        } else {
+            array[k++] = secondhalf[j++];
         }
-        else{
-            array[k++]=secondhalf[j++]
-        }
     }
-    while(i<firsthalf.length){
-        array[k++]=firsthalf[i++]
+
+    while (i < firsthalf.length) {
+        array[k++] = firsthalf[i++];
     }
-    while(j<secondhalf.length){
-        array[k++]=secondhalf[j++]
+
+    while (j < secondhalf.length) {
+        array[k++] = secondhalf[j++];
     }
-};
-const array=[3, 2, 1, 13, 8, 5, 0, 1];
-mergesort(array,0,array.length-1)
-console.log(array)
+}
+
+const array = [3, 2, 1, 13, 8, 5, 0, 1];
+mergesort(array, 0, array.length - 1);
+console.log(array);
